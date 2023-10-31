8f60c27a859b806d208832e57483a085befca479
arukonda-aashish <105883317+arukonda-aashish@users.noreply.github.com>
2023-10-31 06:00:49 +0000

    scoping

---
+++ basics/scopes.js
@@ -0,0 +1,24 @@
+function one()
+{
+    const user="aashish";
+    function two()
+    {
+        var web="github";
+        console.log(user);
+    }
+   // console.log(web);
+    two();
+}
+one();
+//function statement
+function a()
+{
+    console.log("hi");
+}
+a();
+//function expression
+const b=function()
+{
+    console.log("hlo");
+}
+b();
